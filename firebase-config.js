5bdf42fda2a8abc38184994662896994ab334298
UURWEB <talesfromcentraliabook@gmail.com>
2026-06-19 21:40:21 -0500

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBm_3Bf5eczfCLs1tCx07SxS6wjAAIaJTs",
+  authDomain: "webr-5d853.firebaseapp.com",
+  projectId: "webr-5d853",
+  storageBucket: "webr-5d853.firebasestorage.app",
+  messagingSenderId: "171565478949",
+  appId: "1:171565478949:web:a125195339c233b762bbcc",
+  measurementId: "G-F69PDG5W4K"
+};
+
+export const DIRECTORY_COLLECTION = "sites";
+
+export const FIREBASE_CONFIGURED =
+  firebaseConfig.apiKey !== "PASTE_YOUR_API_KEY_HERE" &&
+  firebaseConfig.projectId !== "PASTE_YOUR_PROJECT_ID_HERE";
